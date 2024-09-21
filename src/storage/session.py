65544082c5e61eedcb7dd1bb352a7a9a83a819ac
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-09-21 19:16:14 +0200

    Added session class

---
+++ src/storage/session.py
@@ -0,0 +1,10 @@
+
+class Session:
+    def create_container(self):
+        raise NotImplementedError
+
+    def create_drawer(self):
+        raise NotImplementedError
+
+    def create_component(self):
+        pass
