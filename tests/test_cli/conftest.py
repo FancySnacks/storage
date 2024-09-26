ee99eb73364b8a4ac63f87f7d470df911f6d532e
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-09-26 21:53:46 +0200

    Added CreateArgExecutor tests

--- tests/test_cli/conftest.py
+++ tests/test_cli/conftest.py
@@ -1,9 +1,21 @@
 import pytest
 
 
-ARGV_CREATE_CONTAINER = ['', 'create', 'container', 'test', '8', '8']
+ARGV_CREATE_CONTAINER = ['', 'create', 'container', 'testContainer', '8', '8']
+ARGV_CREATE_DRAWER = ['', 'create', 'drawer', 'testDrawer', 'testContainer']
+ARGV_CREATE_COMPONENT = ['', 'create', 'component', 'testComponent', '5', 'other', 'testContainer', 'testDrawer']
 
 
 @pytest.fixture
 def argv_create_container() -> list[str]:
     return ARGV_CREATE_CONTAINER
+
+
+@pytest.fixture
+def argv_create_drawer() -> list[str]:
+    return ARGV_CREATE_DRAWER
+
+
+@pytest.fixture
+def argv_create_component() -> list[str]:
+    return ARGV_CREATE_COMPONENT
--- tests/test_cli/test_create_argexecutor.py
+++ tests/test_cli/test_create_argexecutor.py
@@ -1,5 +1,31 @@
 from storage.cli.argexecutor import CreateArgExecutor
 
 
-def test_new_container_dir_created(argv_create_container, session):
+def test_new_container_created(argv_create_container, session):
     executor = CreateArgExecutor(session, argv_create_container)
+    executor.parse_args()
+
+    assert len(session.containers) > 0
+
+
+def test_new_drawer_created(argv_create_container, argv_create_drawer, session):
+    executor = CreateArgExecutor(session, argv_create_container)
+    executor.parse_args()
+
+    executor = CreateArgExecutor(session, argv_create_drawer)
+    executor.parse_args()
+
+    assert len(session.containers[-1].drawers) > 0
+
+
+def test_new_component_created(argv_create_component, argv_create_container, argv_create_drawer, session):
+    executor = CreateArgExecutor(session, argv_create_container)
+    executor.parse_args()
+
+    executor = CreateArgExecutor(session, argv_create_drawer)
+    executor.parse_args()
+
+    executor = CreateArgExecutor(session, argv_create_component)
+    executor.parse_args()
+
+    assert len(session.containers[-1].drawers[-1].components) > 0
