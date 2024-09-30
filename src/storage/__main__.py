3346cd8dd0d1f8c533767f7f5e8f19d15862a701
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-09-30 21:04:00 +0200

    Fixes

--- src/storage/__main__.py
+++ src/storage/__main__.py
@@ -8,17 +8,17 @@ from storage.cli.subparser import CreateSubparser, GetSubparser, DeleteSubparser
 from storage.cli.argexecutor import ArgExecutor, CreateArgExecutor, DeleteArgExecutor, ClearArgExecutor, GetArgExecutor
 
 
-def get_arg_executor_from_argv(session, item_type: str, parsed_args: dict) -> ArgExecutor:
-    if 'create' in parsed_args:
+def get_arg_executor_from_argv(session, item_type: str, parsed_args: dict, args: list[str]) -> ArgExecutor:
+    if 'create' in args:
         return CreateArgExecutor(session, item_type, parsed_args)
 
-    if 'get' in parsed_args:
+    if 'get' in args:
         return GetArgExecutor(session, item_type, parsed_args)
 
-    if 'delete' in parsed_args:
+    if 'delete' in args:
         return DeleteArgExecutor(session, item_type, parsed_args)
 
-    if 'clear' in parsed_args:
+    if 'clear' in args:
         return ClearArgExecutor(session, item_type,  parsed_args)
 
     raise ValueError("Cannot initialize a valid subparser!")
@@ -46,7 +46,7 @@ def main(args: list[str] | None = None) -> int:
 
     if len(argv) > 2:
         item_type = argv[2]
-        arg_executor = get_arg_executor_from_argv(session, item_type, parsed_args)
+        arg_executor = get_arg_executor_from_argv(session, item_type, parsed_args, argv)
         arg_executor.parse_args()
     elif len(argv) == 2:
         raise ValueError(f"You attempted to use '{argv[1]}' but did not specify an item: "
